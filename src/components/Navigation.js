dd307a45093215120ecee9da51bc30caf29786ad
Anton Javits <anton.javits@gmail.com>
2020-05-15 01:50:44 +0300

    css

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useState } from 'react';
 import { Navbar, Nav, Button, Form, FormControl } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Navigation() {
     const [signedIn, setSignedIn] = useState(false);
@@ -19,16 +19,25 @@ export default function Navigation() {
         <>
         <Navbar bg="light" expand="lg" collapseOnSelect>
 
-            <Navbar.Brand as={NavLink} to="/" activeClassName="active">Tiimi 4</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Brand as={Link} to="/" activeClassName="active">Tiimi 4</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" >
+            <span>
+                <div id="nav-icon3">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </span>
+            </Navbar.Toggle>
             <Navbar.Collapse id="basic-navbar-nav">
             
                 <Nav className="mr-auto">
-                    <NavLink to="/start" activeClassName="active">Kysely</NavLink>
-                    <NavLink to="/results" activeClassName="active">Tulokset</NavLink>
-                    <NavLink to="/addsurvey" activeClassName="active">Lisää ja muokkaa kyselyitä</NavLink>
-                    <NavLink to="/addquestion" activeClassName="active">Lisää kysymyksiä</NavLink>
-                    <NavLink to="/editquestion" activeClassName="active">Muokkaa kysymyksiä</NavLink>
+                    <Nav.Link eventKey="1" as={Link} to="/start" activeClassName="active">Kysely</Nav.Link>
+                    <Nav.Link eventKey="2" as={Link} to="/results" activeClassName="active">Tulokset</Nav.Link>
+                    <Nav.Link eventKey="3" as={Link} to="/addsurvey" activeClassName="active">Lisää ja muokkaa kyselyitä</Nav.Link>
+                    <Nav.Link eventKey="4" as={Link} to="/addquestion" activeClassName="active">Lisää kysymyksiä</Nav.Link>
+                    <Nav.Link eventKey="5" as={Link} to="/editquestion" activeClassName="active">Muokkaa kysymyksiä</Nav.Link>
                 </Nav>
                 <Nav>
                     <Button variant="outline-primary" onClick={signInOut}>{signedLabel}</Button>
